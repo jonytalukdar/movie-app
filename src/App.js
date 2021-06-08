cbe90bba95c1536ba991468cbc4110d31f3d0bab
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-09 04:12:56 +0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Container } from '@material-ui/core';
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/MainNav';
+import Movies from './components/Pages/Movies/Movies';
+import Search from './components/Pages/Search/Search';
+import Series from './components/Pages/Series/Series';
+import Trending from './components/Pages/Trending/Trending';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Header />
       <div className="app">
         <Container>
@@ -19,7 +23,7 @@ function App() {
         </Container>
       </div>
       <SimpleBottomNavigation />
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import MovieIcon from '@material-ui/icons/Movie';
 import TvIcon from '@material-ui/icons/Tv';
 import SearchIcon from '@material-ui/icons/Search';
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles({
   root: {
@@ -21,6 +21,18 @@ const useStyles = makeStyles({
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const history = useHistory();
+  useEffect(() => {
+    if (value === 0) {
+      history.push('/');
+    } else if (value === 1) {
+      history.push('/movies');
+    } else if (value === 2) {
+      history.push('/series');
+    } else if (value === 3) {
+      history.push('/search');
+    }
+  }, [value, history]);
 
   return (
     <BottomNavigation
