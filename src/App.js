2646959a5ec024294a1e656b08293aa444420fd0
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-09 03:54:08 +0600

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { Container } from '@material-ui/core';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" exact component={Trending} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
