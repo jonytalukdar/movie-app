ee791a568034fb7bc46f0756d8c95dd9cd116026
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-09 17:23:45 +0600

    fix issue

--- src/components/Pages/Trending/Trending.js
+++ src/components/Pages/Trending/Trending.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+require('dotenv').config();
 
 const Trending = () => {
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+
+    setContent(data.results);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
   return (
     <div>
       <span className="page-title">Trending</span>
