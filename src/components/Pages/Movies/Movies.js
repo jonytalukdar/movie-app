1009388d67feea07509c9314703678d2f2608270
jonytalukdar <jonytalukdar30@gmail.com>
2021-06-09 21:45:58 +0600

    completed movies page

--- src/components/Pages/Movies/Movies.js
+++ src/components/Pages/Movies/Movies.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import useGenre from '../../../Hooks/useGenre';
+import Genres from '../../Genres';
 import CustomPagination from '../../Pagination/CustomPagination';
 import SingleContent from '../../SingleContent/SingleContent';
 
@@ -7,10 +9,13 @@ const Movies = () => {
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
   const [numOfPages, setNumOfPages] = useState();
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const genreforURL = useGenre(selectedGenres);
 
   const fetchMovie = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${page}&with_genres=${genreforURL}`
     );
     setContent(data.results);
     setNumOfPages(data.total_pages);
@@ -18,18 +23,29 @@ const Movies = () => {
 
   useEffect(() => {
     fetchMovie();
-  }, [page]);
+    // eslint-disable-next-line
+  }, [page, genreforURL]);
 
   return (
     <div>
       <span className="page-title">Movies</span>
+      <Genres
+        type="movie"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setPage={setPage}
+      />
       <div className="trending">
         {content &&
           content.map((content) => (
             <SingleContent key={content.id} content={content} />
           ))}
       </div>
-      <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
     </div>
   );
 };
